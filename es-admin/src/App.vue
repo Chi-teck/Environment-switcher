1ee0eefffc6dedca3ddfe2986b5b82c1023d3206
Chi-teck <Chi-teck@users.noreply.github.com>
2024-05-11 16:40:56 +0500

    Clean-up

--- es-admin/src/App.vue
+++ es-admin/src/App.vue
@@ -2,18 +2,14 @@
 import Sidebar from './components/Sidebar.vue';
 import Tools from './components/Tools.vue';
 
-export default {
-  components: { Tools, Sidebar },
-};
+export default { components: { Tools, Sidebar } };
 </script>
 
 <template>
   <div class="app">
     <Sidebar class="app__sidebar"/>
     <main class="app__main">
-      <keep-alive include="project">
-        <router-view/>
-      </keep-alive>
+      <router-view/>
     </main>
     <Tools class="app__tools"/>
   </div>
--- popup/popup.js
+++ popup/popup.js
@@ -1,67 +1,65 @@
 async function getCurrentUrl() {
-    const queryOptions = {active: true, currentWindow: true};
-    const [tab] = await chrome.tabs.query(queryOptions);
-    return tab.url;
+  const queryOptions = {active: true, currentWindow: true};
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
 }
 
 function clickHandler(event) {
-    const options = {url:  event.target.dataset.url};
-    event.ctrlKey || event.type === 'auxclick' ?
-        chrome.tabs.create(options) :
-        chrome.tabs.update(null, options);
-    window.close();
+  const options = {url: event.target.dataset.url};
+  event.ctrlKey || event.type === 'auxclick' ?
+    chrome.tabs.create(options) :
+    chrome.tabs.update(null, options);
+  window.close();
 }
 
 function createItem(label, url, active) {
-    const li = document.createElement('li');
-    const button = document.createElement('button')
-    button.innerHTML = label;
-    button.dataset.url = url;
-    button.addEventListener('click', clickHandler)
-    button.addEventListener('auxclick', clickHandler)
-    li.appendChild(button);
-    if (active) {
-        li.classList.add('active');
-    }
-    return li;
+  const li = document.createElement('li');
+  const button = document.createElement('button')
+  button.innerHTML = label;
+  button.dataset.url = url;
+  button.addEventListener('click', clickHandler)
+  button.addEventListener('auxclick', clickHandler)
+  li.appendChild(button);
+  if (active) {
+    li.classList.add('active');
+  }
+  return li;
 }
 
-async function init(data) {
-    const projects = data.projects || [];
+// --------
+const data = await chrome.storage.sync.get();
+const projects = data.projects || [];
 
-    const findProject = function (url) {
-        const checkEnvironment = environment => url.origin === environment.baseUrl;
-        const checkProject = project => project.environments.find(checkEnvironment);
-        return projects.find(checkProject);
-    }
-
-    const currentUrl = new URL(await getCurrentUrl());
-    const project = findProject(currentUrl);
-
-    const list = document.createElement('ul');
-    list.setAttribute('id', 'environments');
-
-    if (project) {
-        const addEnvironmentItem = function (environment) {
-            if (environment.status) {
-                const item = createItem(
-                    environment.name ,
-                    environment.baseUrl +  currentUrl.pathname,
-                    currentUrl.origin === environment.baseUrl,
-                );
-                list.appendChild(item);
-            }
-        }
-        project.environments.forEach(addEnvironmentItem);
-    }
-
-    let path = 'options/index.html';
-    if (project) {
-       path += '#/project/' + project.id
-    }
-    const item = createItem('Options', chrome.runtime.getURL(path), false)
-    list.appendChild(item).classList.add('options')
-    document.body.appendChild(list);
+const findProject = function (url) {
+  const checkEnvironment = environment => url.origin === environment.baseUrl;
+  const checkProject = project => project.environments.find(checkEnvironment);
+  return projects.find(checkProject);
 }
 
-chrome.storage.sync.get().then(init);
+const currentUrl = new URL(await getCurrentUrl());
+const project = findProject(currentUrl);
+
+const list = document.createElement('ul');
+list.setAttribute('id', 'environments');
+
+if (project) {
+  const addEnvironmentItem = function (environment) {
+    if (environment.status) {
+      const item = createItem(
+        environment.name,
+        environment.baseUrl + currentUrl.pathname,
+        currentUrl.origin === environment.baseUrl,
+      );
+      list.appendChild(item);
+    }
+  }
+  project.environments.forEach(addEnvironmentItem);
+}
+
+let path = 'options/index.html';
+if (project) {
+  path += '#/project/' + project.id
+}
+const item = createItem('Options', chrome.runtime.getURL(path), false)
+list.appendChild(item).classList.add('options')
+document.body.appendChild(list);
